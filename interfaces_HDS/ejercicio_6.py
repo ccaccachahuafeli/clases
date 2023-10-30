604be35f7d79b0c95d079733b9f2bce9f753ee34
ccaccachahuafeli <ccaccachahuafeliciana@gmail.com>
2023-10-29 19:48:36 -0500

    realizado actividad suma y resta

--- interfaces_HDS/ejercicio_6.py
+++ interfaces_HDS/ejercicio_6.py
@@ -1,40 +1,66 @@
-from tkinter import *
-ventana=Tk()    
-ventana.geometry("350x300")
-
-def capturar_numeros():
-    usuario =usuario.get()
-    contraseña = contraseña.get()
+from tkinter import*
+ventana=Tk()
 
 
-
-numero1 =Label(ventana,text="ingrese un numero")
-numero1.pack()
-numero1 =Entry(ventana)
-numero1.pack()
+widget_uno=Frame()
+widget_uno.grid(row=6,column=0)
+widget_uno.config(height='30',width='30')
 
 
-numero2 =Label(ventana, text="ingrese un numero")
-numero2.pack()
-numero2 =Entry(ventana)
-numero2.pack()
+et=Label(ventana,text='Ingresa un numero 1:').grid(row=0,column=0)
+num1=Entry()
+num1.grid(row=1,column=0)
 
-total =Label(ventana, text="total")
-total.pack()
-total =Entry(ventana)
-total.pack()
+et=Label(ventana,text='Ingresa un numero 2:').grid(row=2,column=0)
+num2=Entry()
+num2.grid(row=3,column=0)
 
-
-capturar_datos =Button(ventana, text="calcular", command=capturar_numeros)
-capturar_datos.pack()
+et=Label(ventana,text='total').grid(row=4,column=0)
+result=Entry()
+result.grid(row=5,column=0)
 
 
-def suma():
-    n1=int(numero1.get())
-    n2=int(numero2.get())
-    resultado=n1+n2
-    resultado.insert(0,resultado)
-    
-boton_suma=Button(ventana,text='Sumar',command=suma).grid(row=3,column=0)
+
+
+datos=IntVar()
+def operar():
+    n1=int(num1.get())
+    n2=int(num2.get())
+    if datos.get()==1:
+        resultado_suma=n1+n2
+        result.insert(0,resultado_suma)
+    else:
+        resultado_resta=n1-n2
+        result.insert(0,resultado_resta)
+
+    if datos.get()==1:
+        resultado_multicar=n1*n2
+        result.insert(1,resultado_multicar)
+    else:
+        resultado_dividir=n1/n2
+        result.insert(2,resultado_dividir)
+
+
+radios=Radiobutton(ventana,text='suma',value=1,variable=datos)
+radios.grid(row=2,column=1)
+
+radior=Radiobutton(ventana,text='resta',value=0,variable=datos)
+radior.grid(row=3,column=1)
+
+radior=Radiobutton(ventana,text='multilicar',value=2,variable=datos)
+radior.grid(row=4,column=1)
+
+radior=Radiobutton(ventana,text='division',value=3,variable=datos)
+radior.grid(row=5,column=1)
+
+boton_suma=Button(ventana,text='calcular',command=operar).grid(row=7,column=0)
+
+def limpiar():
+    num1.delete(0,END)
+    num2.delete(0,END)
+    result.delete(0,END)
+    num1.focus()
+boton_limpiar=Button(ventana,text='Limpiar',command=limpiar).grid(row=7,column=1)
+
 
 ventana.mainloop()
